06e969626fc16f9bbb8bea20ed38c0af5c56d3c3
Mohammad Faizan <mohammadfaizan0411@gmail.com>
2026-06-12 00:53:29 +0530

    Add files via upload

---
+++ Phase 1 → Foundations/1 → DataRetrievalBasics/DataRetrievalBasics-[4]-10055.sql
@@ -0,0 +1,13 @@
+-- Query
+-- Records with '?'
+-- Find records with the value '?' in the stars column.
+-- ID: 10055
+-- Link: "https://platform.stratascratch.com/coding/10055-records-with?code_type=3"
+
+-- solution
+SELECT 
+      *
+FROM
+     yelp_reviews
+WHERE
+     stars = "?"
