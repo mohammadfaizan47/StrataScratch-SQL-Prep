9dc7255e0541d719f01c6d3db910c8d85015f338
Mohammad Faizan <mohammadfaizan0411@gmail.com>
2026-06-28 01:21:18 +0530

    Update DistinctCount-[Easy]-[1]-9996.sql

--- Phase 1 → Foundations/3 → Distinct Counts/DistinctCount-[Easy]-[1]-9996.sql
+++ Phase 1 → Foundations/3 → Distinct Counts/DistinctCount-[Easy]-[1]-9996.sql
@@ -1,1 +1,11 @@
+-- Query --
+-- Find the average number of streams across all songs --
+-- Find the average number of streams across all songs.
+-- ID: 9996
+-- Link: "https://platform.stratascratch.com/coding/9996-find-the-average-number-of-streams-across-all-songs?code_type=3"
 
+-- solution
+SELECT
+      AVG(streams) AS average_streams
+FROM
+      spotify_worldwide_daily_song_ranking
