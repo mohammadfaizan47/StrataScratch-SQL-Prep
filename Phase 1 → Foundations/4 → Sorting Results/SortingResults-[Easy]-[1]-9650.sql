fc38ac6ce603028998e0e220158e7f7b0cdbcffc
Mohammad Faizan <mohammadfaizan0411@gmail.com>
2026-06-30 01:00:08 +0530

    Update SortingResults-[Easy]-[1]-9650.sql

--- Phase 1 → Foundations/4 → Sorting Results/SortingResults-[Easy]-[1]-9650.sql
+++ Phase 1 → Foundations/4 → Sorting Results/SortingResults-[Easy]-[1]-9650.sql
@@ -1,1 +1,13 @@
+-- Query --
+-- Top 10 songs 2010 --
+-- Find the top 10 ranked songs in 2010. Output the rank, group name, and song name,
+-- but do not show the same song twice. Sort the result based on the rank in ascending order.
+-- ID: 9650
+-- Link: "https://platform.stratascratch.com/coding/9650-find-the-top-10-ranked-songs-in-2010?code_type=3"
 
+-- solution
+SELECT DISTINCT year_rank AS "rank", group_name AS "group name", song_name AS "song name"
+FROM billboard_top_100_year_end
+WHERE year = 2010
+ORDER BY year_rank ASC
+LIMIT 10
